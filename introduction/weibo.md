1f98ce6eb54ce4e972427e32dc8532f000cd3506
312 <1156573707@qq.com>
2020-05-22 19:29:42 +0800

    add weibo_photos_introduction

---
+++ introduction/weibo.md
@@ -0,0 +1,29 @@
+# install
+`
+pip3 install -r requirements.txt
+`
+
+# 介绍
+weibo_photo.py 用requests获得图片的链接存于result.json（非重点）
+weibo_photo_dl.py 用asyncio + aiohttp异步下载result.json中jpg格式的图片
+MYretry.py 利用装饰器实现异步请求的重试（非异步可以使用retrying库）
+
+# 可能的问题
+1.只对client_exceptions.ServerDisconnectedError进行了处理
+若在请求中出现其他异常可以添加到
+@MYretry(client_exceptions.ServerDisconnectedError,otherexception)
+2.不谈weibo_photo.py获取链接是同步方式，
+单是weibo_photo_dl.py也有一种异步和同步代码杂糅的感觉，不知aiofiles这样用是否可以实现异步的文件写入
+
+
+代码不具备通用性，当前的功能是批量下载鞠婧祎的微博相册图片（jpg），
+如需采集其他用户需要改weibo_photo.py得到新的result.json文件
+
+
+# 效果
+经过简单的异常处理没有遗漏的
+并发数CONCURRENCY为5我家的垃圾网络用了400s下载了1500多张图
+（非并发情况下等一张图片的响应就要超过1s）可以说协程的效果还不错
+
+
+
--- weibo_photos/weibo_photo_dl.py
+++ weibo_photos/weibo_photo_dl.py
@@ -9,6 +9,7 @@ from MYretry import *
 
 loop = asyncio.get_event_loop()
 
+
 class Async_download:
     def __init__(self):
         super().__init__()
@@ -16,54 +17,56 @@ class Async_download:
         self.session = None
         self.semaphore = asyncio.Semaphore(self.CONCURRENCY)
         self.save_path = 'result'
-
+    
+    """ 利用python装饰器实现请求的重试 """
     @MYretry(client_exceptions.ServerDisconnectedError)
     async def request(self, url):
         async with self.semaphore:
             # try:
-            print('getting',url)
+            print('getting', url)
             async with self.session.get(url) as response:
                 await asyncio.sleep(1)
                 return await response.read()
             # except client_exceptions.ServerDisconnectedError:
             #     print('ServerDisconnectedError occurred while scraping ',url)
-            
-    def save_pic(self,name,content):
+
+    def save_pic(self, name, content):
         # 同步的写入文件
-        name = os.path.join(self.save_path,name)
-        with open(name,'wb') as f:
+        name = os.path.join(self.save_path, name)
+        with open(name, 'wb') as f:
             f.write(content)
             f.close()
 
-    async def save_pic2(self,name,content):
-        name = os.path.join(self.save_path,name)
+    async def save_pic2(self, name, content):
+        """ 未知是否异步 """
+        name = os.path.join(self.save_path, name)
         async with aiofiles.open(name, mode='wb') as f:
             await f.write(content)
 
-    
-    def get_url_list(self,path):
+    def get_url_list(self, path):
         url_list = []
-        with open(path,'r',encoding='utf-8') as f:
-            res_json = json.loads(f.read()) 
+        with open(path, 'r', encoding='utf-8') as f:
+            res_json = json.loads(f.read())
             for card in res_json:
                 if card['card_type'] == 47:
                     for pic in card['pics']:
-                        if pic['pic_big'][-4:]=='.gif':
+                        if pic['pic_big'][-4:] == '.gif':
                             continue
                         url_list.append(pic['pic_big'])
-            return url_list    
-    
-    async def download_one(self,url,name):
+            return url_list
+
+    async def download_one(self, url, name):
         name = str(name).zfill(4)+'.jpg'
         content = await self.request(url)
-        print('saved',name)
+        print('saved', name)
         if content:
-            await self.save_pic2(name,content)  
+            await self.save_pic2(name, content)
 
-    async def main(self,url_list):
+    async def main(self, url_list):
         self.session = aiohttp.ClientSession()
         # 添加任务一行写法
-        tasks = [asyncio.ensure_future(self.download_one(url_list[i],i)) for i in range(len(url_list))]
+        tasks = [asyncio.ensure_future(self.download_one(
+            url_list[i], i)) for i in range(len(url_list))]
         # tasks = []
         # for i in range(len(url_list)):
         #     tasks.append(asyncio.ensure_future(self.download_one(url_list[i],i)))
@@ -81,4 +84,4 @@ if __name__ == "__main__":
     start_time = time.time()
     spider = Async_download()
     spider.run()
-    print('cost',time.time()-start_time)
+    print('cost', time.time()-start_time)
