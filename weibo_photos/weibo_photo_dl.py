140fb9cfef998fb3996d036bb890b3fe28f06aaa
skygongque <1243650225@qq.com>
2021-01-06 20:06:25 +0800

    separate async from common code

--- weibo_photos/weibo_photo_dl.py
+++ weibo_photos/weibo_photo_dl.py
@@ -11,15 +11,15 @@ loop = asyncio.get_event_loop()
 
 
 class AsyncDownloader:
-    def __init__(self):
+    def __init__(self,save_path):
         super().__init__()
         self.CONCURRENCY = 5
         self.session = None
         self.semaphore = asyncio.Semaphore(self.CONCURRENCY)
-        self.save_path = 'result'
+        self.save_path = save_path
     
     """ 利用python装饰器实现请求的重试 """
-    @asyncFunctionRetry(client_exceptions.ServerDisconnectedError)
+    @asyncFunctionRetry(client_exceptions.ServerDisconnectedError,client_exceptions.ClientPayloadError)
     async def request(self, url):
         async with self.semaphore:
             print('getting', url)
@@ -28,39 +28,20 @@ class AsyncDownloader:
                 await asyncio.sleep(1)
                 return await response.read()
 
-    def save_pic(self, name, content):
-        # 同步的写入文件
-        name = os.path.join(self.save_path, name)
-        with open(name, 'wb') as f:
-            f.write(content)
-            f.close()
-
-    async def save_pic2(self, name, content):
+    async def save_pic(self, name, content):
         """ 未知是否异步 """
         name = os.path.join(self.save_path, name)
         async with aiofiles.open(name, mode='wb') as f:
             await f.write(content)
-
-    def get_url_list(self, path):
-        url_list = []
-        with open(path, 'r', encoding='utf-8') as f:
-            res_json = json.loads(f.read())
-            for card in res_json:
-                if card['card_type'] == 47:
-                    for pic in card['pics']:
-                        if pic['pic_big'][-4:] == '.gif':
-                            continue
-                        url_list.append(pic['pic_big'])
-            return url_list
 
     async def download_one(self, url, name):
         name = str(name).zfill(4)+'.jpg'
         content = await self.request(url)
         print('saved', name)
         if content:
-            await self.save_pic2(name, content)
+            await self.save_pic(name, content)
 
-    async def main(self, url_list):
+    async def asyncTasks(self, url_list):
         self.session = aiohttp.ClientSession()
         # 添加任务一行写法
         tasks = [asyncio.ensure_future(self.download_one(
@@ -71,15 +52,27 @@ class AsyncDownloader:
         await asyncio.gather(*tasks)
         await self.session.close()
 
-    def run(self):
-        if not os.path.exists(self.save_path):
-            os.makedirs(self.save_path)
-        url_list = self.get_url_list('result.json')
-        loop.run_until_complete(self.main(url_list))
+
+def get_url_list(path):
+    url_list = []
+    with open(path, 'r', encoding='utf-8') as f:
+        res_json = json.loads(f.read())
+        for card in res_json:
+            if card['card_type'] == 47:
+                for pic in card['pics']:
+                    if pic['pic_big'][-4:] == '.gif':
+                        continue
+                    url_list.append(pic['pic_big'])
+        return url_list      
+
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    spider = AsyncDownloader()
-    spider.run()
+    save_path = 'result'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    ad = AsyncDownloader(save_path)
+    url_list = get_url_list('result.json')
+    loop.run_until_complete(ad.asyncTasks(url_list))
     print('cost', time.time()-start_time)
